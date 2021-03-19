0d72181da6388b83ffb92e3445650de0173b776e
akimisc <akimiscarcella@akimismacbookpro.local>
2021-03-19 17:23:32 +0100

    Palindromes exercise completed

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function() {
+function palindromes(rawSentence) {
+    let isAPalindrome = true;
 
+    let rawishSentence = rawSentence
+                            .replaceAll(".", "")
+                            .replaceAll(",", "")
+                            .replaceAll("!", "")
+                            .replaceAll(" ", "");
+    let sentence = rawishSentence.toLowerCase();
+
+    for (let i = 0; i < sentence.length; i++) {
+        let firstishCharacter = sentence.charAt(i);
+        let lastishCharacter = sentence.charAt(sentence.length - i - 1);
+
+        isAPalindrome = (firstishCharacter === lastishCharacter);
+
+        let isNotAPalindrome = (firstishCharacter !== lastishCharacter);
+        if (isNotAPalindrome) {
+            break;
+        }
+    }
+    return isAPalindrome;
 }
 
 module.exports = palindromes
