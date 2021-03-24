490e0cb0018f238bd94806fdb38a87865d807e53
akimisc <akimiscarcella@akimismacbookpro.local>
2021-03-24 12:54:18 +0100

    GetTheTitles exercise completed

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+function getTheTitles(books) {
+    let titlesList = [];
 
+    for (let i = 0; i < books.length; i++) {
+        titlesList.push(books[i].title);
+    }
+    return titlesList;
 }
 
 module.exports = getTheTitles;
