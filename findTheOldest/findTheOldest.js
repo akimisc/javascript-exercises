4594747662b1530272e22f0fe24cfb40096e7df9
akimisc <akimiscarcella@akimismacbookpro.local>
2021-03-24 13:46:55 +0100

    FindTheOldest exercise completed

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-let findTheOldest = function() {
+function compareAge(firstPerson, secondPerson) {
+    return firstPerson.age > secondPerson.age ? -1 : 1;
+}
+
+function findTheOldest(people) {
+
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath === undefined) {
+            people[i].yearOfDeath = new Date().getFullYear();
+        }
+        people[i].age = people[i].yearOfDeath - people[i].yearOfBirth;
+
+    }
+
+    people.sort(compareAge);
+    return people[0];
 
 }
 
