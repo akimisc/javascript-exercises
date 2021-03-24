4a19ab69a32468efe1fdd6acb8911bfaa8a68877
akimisc <akimiscarcella@akimismacbookpro.local>
2021-03-24 12:34:32 +0100

    Fibonacci exercise completed

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+function fibonacci(targetPosition) {
+    let firstishNumber = 0;
+    let lastishNumber = 1;
+
+    if (targetPosition < 0) {
+        return "OOPS";
+    }
+
+    for (let position = 1; position < targetPosition; position++) {
+        let nextNumber = firstishNumber + lastishNumber;
+        firstishNumber = lastishNumber;
+        lastishNumber = nextNumber;
+    }
+    return lastishNumber;
 
 }
 
